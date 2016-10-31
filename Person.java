e4086de7c8b202c69c99a1541b001d03c3ba5ea8
RLOG89 <ross.loggie@gmail.com>
2016-10-31 15:46:39 +0000

    initial commit

---
+++ Person.java
@@ -0,0 +1,11 @@
+public class Person{
+  private String name;
+  private Pint[] drunk;
+
+  public Person(String, name){
+    this.name = name;
+    this.drunk = new Pint[10];
+  }
+
+
+}
---
+++ PersonTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class PersonTest{
+  Person person;
+  Pint pint;
+
+  @Before 
+  public void before(){
+    person = new Person("Tom");
+    pint = new Pint();
+  }
+
+  @Test
+  public void hasName(){
+    assertEquals("Tom", person.hasName)
+  }
+
+
+}
